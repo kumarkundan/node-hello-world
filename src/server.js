b049fe8996bd48fee2da476495dddac849b698f7
Kumar Kundan <kkajnabi.kumar@gmail.com>
2020-01-09 12:40:46 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello KundanJee World\n');
+  res.send('Hello Kundan Jee World\n');
 });
 app.get('/:name', (req, res) => {
   res.send('Hello '+req.params.name+'!\n');
